f1f7e0a71ceec4dd6314cbeaa0817e9dc5eaf49d
James E. Carter <sitjec@US.Int.SonicHealthcare>
2017-12-30 22:46:35 -0600

    finished main 1st of the month

--- CodeFolder/AutomateSORs.py
+++ CodeFolder/AutomateSORs.py
@@ -7,12 +7,16 @@ import datetime
 #This program assumes that the csv file has a header row
 #It also assumes that all of their input extracts' headers end with the months header columns always ending in DEC
 
+#12/30/2017:
+#completely correctly does the main kidnof file that you find in 1st of the month...but i wish there was a way to make this work for creating .xls files
+
 #To DO TODO:
 #Make it send an email if it fails...I have an unwritten function sendErrorMessage() for that purpose
 #Currenty this program assumes that we are working off of its own directory
 #   a function to bring other extracts into its directory might need to be added
 
 #DIfferent types of extract formats
+#if there was a way to create an excel file out of it.
 
 todayDate = datetime.datetime.now()
 todayYear = todayDate.year
@@ -29,7 +33,7 @@ def initializeListOfLists(csvList):
 def storeCSVAsList(fileName,outputList):
     del outputList[:]
     with open(fileName,'r') as f:
-        csv_f = csv.reader(f)
+        csv_f = csv.reader(f, delimiter = '\t')
         for row in csv_f:
             outputList.append(row)
 
@@ -58,7 +62,7 @@ def manipulateTypicalMonthly():
 #                     t: SM1 SM2 SM3 SM4 SM5 SM6
 #"""
     originalRowLength = 0
-    rowLength = 0
+#    rowLength = 0
     monthTotals = []
     
     if todayMonth == 1:
@@ -70,7 +74,6 @@ def manipulateTypicalMonthly():
         yearBeingProcessed = todayYear - 1
     else:
         yearBeingProcessed = todayYear
-    print(monthBeingProcessed)
 
     for i in range(1, monthBeingProcessed +2):    #1 element for each month and also for YTD
         monthTotals.append("")
@@ -78,77 +81,98 @@ def manipulateTypicalMonthly():
     originalRowLength = len(currentCSVList[0])
 
     #This 'if' block makes sure extract follows convention of having a header end with the months header columns always ending in DEC
-    if currentCSVList[0][rowLength-1][0:1].lower != "dec":
+    if currentCSVList[0][originalRowLength - 1][0:3].lower() != "dec":
         sendErrorMessage()
         return
-    if currentCSVList[0][rowLength-2][0:1].lower != "nov":
+    if currentCSVList[0][originalRowLength - 2][0:3].lower() != "nov":
         sendErrorMessage()
         return
     
-    rowLength = originalRowLength - (12 - todayMonth)
+    #rowLength = originalRowLength - (12 - todayMonth)
 
-    #Let's delete all columns fromoriginal extract that we dont' need...columns after the one for monthBeingProcessed
+    #Let's delete all columns fromoriginal extract that we dont' need...columns after the one for monthBeingProcessed and add the YTD column after that.
     for i in range(0,len(currentCSVList)):
         for j in range(originalRowLength - 12 + (monthBeingProcessed - 1) + 1,originalRowLength):
             del currentCSVList[i][originalRowLength - 12 + (monthBeingProcessed - 1) + 1:]
+        if i == 0:              #Here add the extra column for YTD
+            currentCSVList[i].append("YTD" + str(yearBeingProcessed))
+        else:
+            currentCSVList[i].append("")
+
+    #After reshaping the list of lists we reevaulte these variables
+    finalRowLength = originalRowLength - 12 + monthBeingProcessed + 1
+    indexOfFirstMonth = originalRowLength - 12
+
+    #Here in all cells where there should be number values...if the cell is blank then we input a zero.  This is to avoid problems in the program later on
+    for i in range(1,len(currentCSVList)):
+       for j in range(indexOfFirstMonth,finalRowLength - 1):
+           if currentCSVList[i][j] == "":
+               currentCSVList[i][j] = "0"       #we make it a string because other values in csv they are string
 
 
-    currentCSVList[0].append("YTD" + str(yearBeingProcessed))
-    rowLength += 1
+    #rowLength += 1
     if len(currentCSVList) > 1:
         for i in range(1,len(currentCSVList)):
-            currentCSVList[i].append("")
             ytdSum = 0
-            for j in range(originalRowLength - 12,rowLength - 1):
+            for j in range(indexOfFirstMonth, finalRowLength - 1):
                 #the if-else section below needs to convert string ot a number or it won't add rather than concatenate a string.  converts to either int or float.  If we happen to hcomeacross a situation where int + float it dwon't mateter becuase it cast automatically to float.
                 if currentCSVList[i][j].find(".")!= -1:
                     tempNumber = float(currentCSVList[i][j])
                 else:
                     tempNumber = int(currentCSVList[i][j])
                 ytdSum += tempNumber
-            currentCSV[i][rowLength-1] = ytdSum
+            currentCSVList[i][finalRowLength - 1] = str(ytdSum)
     else:
         pass
 
 
     #get sums for each individual month column and also YTD column:
-    for i in range(0,len(monthsTotals)):    #do each month in monthsTotals
+    for i in range(0,len(monthTotals)):    #do each month in monthTotals
         tempTotal = 0
         for j in range(1,len(currentCSVList)):  #go down each row for the column corresponding to the month of the outer loop
-            tempNumber = currentCSVList[j][originalRowLength-12+i]
-            if currentCSVList[i][j].find(".")!= -1:
+            tempNumber = currentCSVList[j][indexOfFirstMonth + i]
+
+            if currentCSVList[j][indexOfFirstMonth + i].find(".")!= -1:
                 tempNumber = float(tempNumber)
             else:
                 tempNumber = int(tempNumber)
             tempTotal += tempNumber
-        monthsTotals[i] = tempTotal
+        monthTotals[i] = tempTotal
 
 
 
 
     #in the section below we add the last row which will have the totals:
     tempRow = []
-    for j in range(0,rowLength):
+    for j in range(0,finalRowLength):
         tempRow.append("")
-    currentCSVList.append(tempRow)
-    currentCSVList.append(tempRow)
+    currentCSVList.append(tempRow[:])
+    currentCSVList.append(tempRow[:])
     try:
-        currentCSVList[len(currentCSVList)-1][originalRowLength - 12 - 1] = "TOTALS:"
+        currentCSVList[len(currentCSVList)-1][indexOfFirstMonth - 1] = "TOTALS:"
     except:
         generalExceptionHandler()
 
+    for i in range(0,len(monthTotals)):
+        monthTotals[i] = str(monthTotals[i])
+        if monthTotals[i].find(".") == -1:
+            monthTotals[i] = int(monthTotals[i])
+            monthTotals[i] = "{:,}".format(monthTotals[i])
+        else:
+            monthTotals[i] = float(monthTotals[i])
+            monthTotals[i] = "{:,}".format(monthTotals[i])
+
+
     tempIndex = 0
-    for i in range(originalRowLength-12, rowLength):
+    for i in range(indexOfFirstMonth, finalRowLength):
         currentCSVList[len(currentCSVList) - 1][i] = monthTotals[tempIndex]
         tempIndex += 1
 
-    #print(len(currentCSVList))
-    print("in function")
+
 
 def generalExceptionHandler():
     pass #sendEmailthing
 
-                  
 
 def sendErrorMessage():
     pass
@@ -157,12 +181,11 @@ def sendErrorMessage():
 #---------------------------------------------------------------------
 #---------------------------------------------------------------------
 #THSI IS THE MAIN SECTION
-
 initializeListOfLists(currentCSVList)
-storeCSVAsList('flexsite_uc_stats.txt',currentCSVList)
+storeCSVAsList('flexsite_uc_stats.csv',currentCSVList)
 manipulateTypicalMonthly()
 writeListToCSV(currentCSVList,'flex_test.csv')
-#print(currentCSVList)
-print(len(currentCSVList))
+
 #---------------------------------------------------------------------
+
 #---------------------------------------------------------------------
