ef9f723e0696344290ca6431fb7cf784e5dde24c
James E. Carter <sitjec@US.Int.SonicHealthcare>
2018-01-16 11:57:45 -0600

    all done..just need to put into linux and add error handlers

---
+++ codefolder/phase7/nothing.py
@@ -0,0 +1,28 @@
+#what I learned:
+    #there is cloud based mining...soem where you contract some to mine for you and some where you become part of a farm
+
+    #5 types of wwallets:
+        #online, hardware, core, desktop, paper
+
+        
+forking...when a coin when some peopel there decide that they will use some other kidn of confirmation...and when it turns out that that new type of confirmation confirmatios soem transacations whereas the old kidn don't then we have a new kidn of coin or a split of that coin.
+    peopel don't lose value because of that
+
+$different types of alt coin:
+
+Litecoin: thsi is is meant to be like bitcoin....it uses a slightly different crypt...it has a maximum of 84 million coin that will ever be mined.
+There's etherium...not sure how this oen works...somethign like teh amount doled otu per day oid controlled...and that theres' somethign like a inflation based type thing....It is able to be used on the etherium network
+    there's alot of possbility for programmers here and stuff..not sure how...there 's somethin in this called DAB?...or DAO?...I forgot.
+
+    There's dash: used to be called dark and then x something.
+
+    Ripple: this one is meant to be used for banking and they're hoping that they will be your mother's currency
+
+    There esteem...which lets peope mine coin based on the content that they put on this social media.  ...I think that the more we contribute the more weight your votes have ???
+
+    hwo trading in crypto is different:
+        1. 24-7
+        2. very very volatile
+        3. unregulated and wild west type stuff
+            a. an example ois this:...when a coin is all of the sudden added ot a new market liek a new exchange..thsi in and of itself causes the price of that alt coin to change so much
+            b. sometimes 2 coins are tied together in some way.
